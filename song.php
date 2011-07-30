083d51f86c835944b6e56be3d887a5623cb2593d
rintaun <rintaun@gmail.com>
2011-07-30 00:18:29 -0400

    Begin base for song.php

--- song.php
+++ song.php
@@ -1,2 +1,5 @@
 <?php
-print_r($_GET);
+$base = preg_replace('!/song.php$!i', '', preg_replace("!^{$_SERVER['DOCUMENT_ROOT']}!i", '', $_SERVER['SCRIPT_FILENAME']));
+if (!$sid = preg_replace("!^{$base}/song/(.+)!i", "\\1", $_SERVER['REQUEST_URI']))
+	die();
+echo "Song ID: " . $sid;
